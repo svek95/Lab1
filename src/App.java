d8df1d21e42d25c1c7c4e6a0fdf149bb3c8d5ff5
SVEK <svek.95@gmail.com>
2019-02-23 20:12:20 +0300

    add gitignore

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String url = "http://localhost:8080/PersonService";
+        String url = "http://localhost:8080/PersonWebService";
         Endpoint.publish(url, new PersonWebService());
     }
 }
